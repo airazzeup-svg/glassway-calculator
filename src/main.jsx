8561ec32b5d8301df4938dc7ad951fe57c060a2a
airazzeup-svg <airazzeup@gmail.com>
2026-06-29 11:48:55 +0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CeilingCalculator from './CeilingCalculator.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CeilingCalculator />
+  </React.StrictMode>,
+)
